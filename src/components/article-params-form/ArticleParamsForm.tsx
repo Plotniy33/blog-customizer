7cc268b061f956e99c00bb3d45e862e4e1f32840
Plotniy33 <plotnikoffvolodimir@gmail.com>
2024-04-24 12:30:20 +0300

    arrow button stories test

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,12 +1,14 @@
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
 
+import { useState } from 'react';
 import styles from './ArticleParamsForm.module.scss';
 
 export const ArticleParamsForm = () => {
+	const [openForm, setOpenForm] = useState<boolean>(false);
 	return (
 		<>
-			<ArrowButton />
+			<ArrowButton isOpen={openForm} onClick={() => setOpenForm}/>
 			<aside className={styles.container}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
